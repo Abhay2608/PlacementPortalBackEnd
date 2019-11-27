e22007ed60b852cd0104ce7954ae68bb019940d8
agrawal-utkarsh <14103022utkarsh@gmail.com>
2019-11-27 18:35:45 +0530

    added addAdmin utility

---
+++ src/main/java/iiitb/placement_portal/utility/AddAdminUtility.java
@@ -0,0 +1,31 @@
+package iiitb.placement_portal.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+class AddAdminUtility{  
+
+	public static void main(String args[]){  
+	try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/placement_portal","ooad","root");  
+
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			
+		String query = "insert into user(id,name,password)values(?,?,?)";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		preparedStmt.setInt(1,14);
+//	    preparedStmt.setString(2,passwordEncoder.encode("password"));
+	    preparedStmt.setString(2,"roshni");
+	    preparedStmt.setString(3,"password" );
+	    preparedStmt.executeUpdate();
+	    
+	    con.close();  
+	    System.out.println(" main class - Admin Insertion Successful ");
+		}
+	catch(Exception e){ 
+		System.out.println(" main class - exception " + e);
+		}  
+	}  
+}
