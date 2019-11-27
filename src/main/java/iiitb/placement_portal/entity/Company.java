58b07e9bcf8d44d33589756be715cc7dccaedda3
Abhay2608 <abhaynanda94@gmail.com>
2019-11-28 05:11:29 +0530

    added domain and profile field in company entity

--- src/main/java/iiitb/placement_portal/entity/Company.java
+++ src/main/java/iiitb/placement_portal/entity/Company.java
@@ -31,21 +31,129 @@ public class Company {
 	private ArrayList<String> streamRequirement;
 	private ArrayList<CompanyContacts> contact;
 	private ArrayList<Boolean> type;	//summer intern, intern, full time, I+F
+	private String domain;
+	private String profile;
 
-	public Company(Integer id, String name, ArrayList<CompanyContacts> contact, String jd, Date opentime,
-				   Date closetime, ArrayList<String> courseRequirement, ArrayList<String> streamRequirement,
-				   float cgpaRequired, ArrayList<Boolean> type, String contactInString) {
-		super();
+	public Company(Integer id, String name, String jd, Date opentime, Date closetime, float cgpaRequired, String contactInString, ArrayList<String> courseRequirement, ArrayList<String> streamRequirement, ArrayList<CompanyContacts> contact, ArrayList<Boolean> type, String domain, String profile) {
 		this.id = id;
 		this.name = name;
-		this.contact = contact;
-		this.contactInString = contactInString;
 		this.jd = jd;
 		this.opentime = opentime;
 		this.closetime = closetime;
+		this.cgpaRequired = cgpaRequired;
+		this.contactInString = contactInString;
 		this.courseRequirement = courseRequirement;
 		this.streamRequirement = streamRequirement;
-		this.cgpaRequired = cgpaRequired;
+		this.contact = contact;
 		this.type = type;
+		this.domain = domain;
+		this.profile = profile;
+	}
+
+	public Company() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJd() {
+		return jd;
+	}
+
+	public void setJd(String jd) {
+		this.jd = jd;
+	}
+
+	public Date getOpentime() {
+		return opentime;
+	}
+
+	public void setOpentime(Date opentime) {
+		this.opentime = opentime;
+	}
+
+	public Date getClosetime() {
+		return closetime;
+	}
+
+	public void setClosetime(Date closetime) {
+		this.closetime = closetime;
+	}
+
+	public float getCgpaRequired() {
+		return cgpaRequired;
+	}
+
+	public void setCgpaRequired(float cgpaRequired) {
+		this.cgpaRequired = cgpaRequired;
+	}
+
+	public String getContactInString() {
+		return contactInString;
+	}
+
+	public void setContactInString(String contactInString) {
+		this.contactInString = contactInString;
+	}
+
+	public ArrayList<String> getCourseRequirement() {
+		return courseRequirement;
+	}
+
+	public void setCourseRequirement(ArrayList<String> courseRequirement) {
+		this.courseRequirement = courseRequirement;
+	}
+
+	public ArrayList<String> getStreamRequirement() {
+		return streamRequirement;
+	}
+
+	public void setStreamRequirement(ArrayList<String> streamRequirement) {
+		this.streamRequirement = streamRequirement;
+	}
+
+	public ArrayList<CompanyContacts> getContact() {
+		return contact;
+	}
+
+	public void setContact(ArrayList<CompanyContacts> contact) {
+		this.contact = contact;
+	}
+
+	public ArrayList<Boolean> getType() {
+		return type;
+	}
+
+	public void setType(ArrayList<Boolean> type) {
+		this.type = type;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
 	}
 }
